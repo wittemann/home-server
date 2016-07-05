73be9b1094835322277ff6890e7d22e07cc39ac5
wittemann <martin.wittemann@1und1.de>
2016-07-05 17:42:47 +0200

    make scenes triggerable

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const dropbox = require('./dropbox.js');
+const everhome = require('./everhome.js');
 
 const move = require('./action/move.js');
 
-dropbox.check(function(msg) {
+dropbox.check((msg) => {
   if (msg.type === 'move') {
     move(msg.data);
+  } else if (msg.type === 'scene') {
+    everhome.scene(msg.data);
   }
 });
